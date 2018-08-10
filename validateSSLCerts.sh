a01c6a53bff8d3d8b1c38b517d0737222045236d
Matt <carlotta.matt@gmail.com>
2018-08-09 20:45:16 -0700

    Update comments and tweak some log messages

--- validateSSLCerts.sh
+++ validateSSLCerts.sh
@@ -19,8 +19,9 @@
 #===============================================================================##
 ## TODO                                                                          #
 ##==============================================================================##
-# 1. Update README with instructions and include Synology + Gitlab docker gist
-# 2. Research if Synology NAS autorenews invalidated certs; if not, research how to force renew after X days before
+# 1. Add commands to overwrite global variables
+# 2. Update README with instructions
+# 3. Research if Synology NAS autorenews invalidated certs; if not, research how to force renew after X days before
 
 
 #===============================================================================##
@@ -98,14 +99,14 @@ function _create_new_certs()
 function _remove_old_certs()
 {
 	if [ ! -f "$gCertPath"/cert.pem ] || [ ! -f "$gCertPath"/gitlab.key ] || [ ! -f "$gCertPath"/gitlab.key ];
-		then
-			_abort_session "Unable to locate your current certifications in $gCertPath."
+	then
+	_abort_session "Unable to locate your current certifications in $gCertPath."
 	fi
 
 	rm "$gCertPath"/cert.pem "$gCertPath"/gitlab.key "$gCertPath"/gitlab.crt > /dev/null 2>&1
 	if [[ $? -ne 0 ]];
-		then
-			_abort_session "Unable to remove your current certifications."
+	then
+	_abort_session "Unable to remove your current certifications."
 	fi
 	printf "$gCurrentTime -- Removed the old certificates from $gCertPath. \n" 																																																							>> "$gLogPath"
 }
@@ -120,11 +121,11 @@ function _restart_gitlab_container
 	cd "$gitlabPath"
 	docker-compose restart gitlab > /dev/null 2>&1
 	if [[ $? -ne 0 ]];
-		then
-			printf "$gCurrentTime -- Uh oh. Gitlab has failed to restart! Check your docker logs to find out why.\n" 																																																			>> "$gLogPath"
-			printf "$gCurrentTime -- Aborting session.\n" 																																																	>> "$gLogPath"
-			_end_session
-			exit 1
+	then
+	printf "$gCurrentTime -- Uh oh. Gitlab has failed to restart! Check your docker logs to find out why.\n" 																																																			>> "$gLogPath"
+	printf "$gCurrentTime -- Aborting session.\n" 																																																	>> "$gLogPath"
+	_end_session
+	exit 1
 	fi
 	printf "$gCurrentTime -- Everything looks good. You should be up and running in about 5 minutes.\n" 																																																			>> "$gLogPath"
 }
@@ -151,22 +152,22 @@ function _expired_certs() {
 
 
 #===============================================================================##
-## VALIDATE CERTS -- ATTEMTPS TO VALIDATE LETS ENCRYPT CERTIFICATIONS            #
+## VALIDATE CERTS -- ATTEMPTS TO VALIDATE LETS ENCRYPT CERTIFICATIONS            #
 ##==============================================================================##
 function _validate_certs()
 {
 	local checkCertStatus=$(/bin/openssl x509 -checkend $(( 86400 * gCertExpireDays )) -in $gCertPath/cert.pem)
 
 	if [[ $checkCertStatus == "Certificate will not expire" ]];
-		then
-			_show_valid_dates
-			printf "$gCurrentTime -- No need to renew your certifications! \n" 																																																					>> "$gLogPath"
+	then
+	_show_valid_dates
+	printf "$gCurrentTime -- No need to renew your certifications! \n" 																																																					>> "$gLogPath"
 	else
-		_show_valid_dates
-		_expired_certs
-		_remove_old_certs
-		_create_new_certs
-		_restart_gitlab_container
+	_show_valid_dates
+	_expired_certs
+	_remove_old_certs
+	_create_new_certs
+	_restart_gitlab_container
 	fi
 }
 
@@ -177,13 +178,13 @@ function _validate_certs()
 function _check_paths
 {
 	if [ ! -d "$gLetsEncryptPath" ];
-		then
-			_abort_session "Unable to locate the Let's Encrypt certifications path."
+	then
+	_abort_session "Unable to locate the Let's Encrypt certifications path. The directory does not exist."
 	fi
 
 	if [ ! -d "$gCertPath" ] || [ ! -f "$gCertPath"/cert.pem ];
-		then
-			_abort_session "Unable to locate the cert.pem file in $gCertPath."
+	then
+	_abort_session "Unable to locate the cert.pem file in $gCertPath. The directory and/or file does not exist."
 	fi
 }
 
@@ -204,9 +205,9 @@ function _begin_log_session
 function _create_log_file
 {
 	if [ ! -f "$gLogPath" ];
-		then
-			touch "$gLogPath"
-			chown -R 1000:1000 "$gLogPath"
+	then
+	touch "$gLogPath"
+	chown -R 1000:1000 "$gLogPath"
 	fi
 }
 
@@ -219,16 +220,16 @@ function _check_log_size
 	local logSize=$(/bin/stat -c%s "$gLogPath")
 
 	if ((logSize > gLogMaxSize));
-		then
-			rm $gLogPath
-			_create_log_file
-			printf "%s--------------------------- LOG FILE WAS TRIMMED ON $gCurrentDate @ $gCurrentTime --------------------------- \n\n" 																																																					>> "$gLogPath"
+	then
+	rm $gLogPath
+	_create_log_file
+	printf "%s--------------------------- LOG FILE WAS TRIMMED ON $gCurrentDate @ $gCurrentTime --------------------------- \n\n" 																																																					>> "$gLogPath"
 	fi
 }
 
 
 #===============================================================================##
-## MAIN -- RUNS MAIN SCRIPT #
+## MAIN -- RUNS MAIN SCRIPT                                                      #
 ##==============================================================================##
 function main
 {
@@ -243,18 +244,18 @@ function main
 
 
 #===============================================================================##
-## ENTRY -- CHECK IF FILE IS ROOT #
+## ENTRY -- CHECK IF FILE IS ROOT                                                #
 ##==============================================================================##
 if [[ `id -u` -ne 0 ]];
-  then
-    clear
-    printf "This script must be run as the ROOT USER! Make sure the script has the correct root permissions.\n" 		>> "$gLogPath"
-    exit 1
-  else
-    main
+	then
+		clear
+		printf "This script must be run as the ROOT USER! Make sure the script has the correct root permissions.\n" 		>> "$gLogPath"
+		exit 1
+	else
+		main
 fi
 
 
 #===============================================================================##
-## EOF #
+## EOF                                                                           #
 ##==============================================================================##
